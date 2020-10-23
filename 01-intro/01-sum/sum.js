92414eff18dc74f831d35cba6babb7c4cb2d417c
Eugene <melnikov_e@list.ru>
2020-10-23 17:48:05 +0300

    add solution for 1-module 1-task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,14 @@
 function sum(a, b) {
-  /* ваш код */
+  let a_num, b_num;
+  try {
+    if ((typeof a !== 'number') || (typeof b !== 'number'))  {
+      throw new TypeError();
+    } else {
+      return Number(a) + Number(b);
+    }
+  } catch (e) {
+    throw new TypeError();
+  }
 }
 
 module.exports = sum;
